d8b4e8d2fdedb55340272eaf0f6373ee5591e771
Himanshu Yadav <himanshu.yadav@giva.co>
2025-05-17 14:19:43 +0530

    count app project done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_1';
+ count = 0;
+
+
+  ngOnInit(): void {
+    this.count = 0;
+  }
+
+ increment(){
+  this.count++;
+
+ }
+
+ decrement(){
+  this.count--;
+ }
+
+ reset(){
+  this.count = 0;
+ }
+
+
 }
